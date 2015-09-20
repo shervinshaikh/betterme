11bfe3ed45a5e39bf6d0eb192528db770216fec5
Shervin Shaikh <shervinater@gmail.com>
2015-09-19 18:43:30 -0700

    Add database firebase

---
+++ firebase.js
@@ -0,0 +1,84 @@
+var Firebase = require('firebase');
+var fbRef = new Firebase('https://betterme-data.firebaseio.com/');
+
+var usersRef = fbRef.child('Users');
+var remindersRef = fbRef.child('Reminders');
+var followUpsRef = fbRef.child('FollowUp');
+
+module.exports = {
+  // true/false if succeeded
+  createUser: function(phoneNumber){
+    var user = {
+      phoneNumber: phoneNumber.toString(),
+      name: null,
+      email: null
+      // currentState: 'registered' // or 0? do we need this?
+    };
+    usersRef.child(phoneNumber).set(user, isDataSaved);
+  },
+  setName: function(phoneNumber, name){
+    usersRef.child(phoneNumber).update({
+      name: name
+    }, isDataSaved);
+  },
+  setEmail: function(phoneNumber, email){
+    usersRef.child(phoneNumber).update({
+      email: email
+    }, isDataSaved);
+  },
+  // var user = {
+  //   phoneNumber: '9494194999',
+  //   name: 'Shervin Shaikh',
+  //   email: 'shervin@gmail.com',
+  // }
+  getUser: function(phoneNumber){
+    // TODO
+    // usersRef.on('value', function(snap){
+    //   return snap.val();
+    // });
+    // return 'hello';
+  },
+  // var reminder = {
+  //   phoneNumber: '9494194999',
+  //   text: 'take birth control',
+  //   interval: 86400000,
+  //   sendTime: new Date().getTime(),
+  //   state: 0, // 0 - reminder, 1 - first follow up, 2 - second follup up, etc
+  // }
+  createReminder: function(reminder){
+    remindersRef.push(reminder);
+  },
+  createFollowUp: function(reminder){
+    followUpsRef.push(reminder);
+  },
+  getRemindersAtTime: function(time){
+    // remindersRef.on('child_added', function(snap){
+    //   var reminder = snap.val();
+
+    //   // TODO!
+    //   snap.ref().remove();
+    // });
+  },
+  getFollowUpsAtTime: function(time){
+  //   followUpsRef.on('child_added', function(snap){
+  //     var reminder = snap.val();
+
+  //     // TODO!
+  //     snap.ref().remove();
+  //   });
+  }
+};
+
+function isDataSaved(e){
+  if(e){
+    console.warn('Data could not be saved');
+  } else {
+    // console.log('Data saved succesfully');
+  }
+}
+
+
+// Create function that is not accesible outsite
+var doSomethingInternal = function () {
+  // do some
+};
--- server.js
+++ server.js
@@ -1,8 +1,18 @@
-var twilioWrapper = require("./twilio");
 var express = require('express');
 var bodyParser = require('body-parser');
-var app = express();
+var cron = require('cron');
+var Firebase = require('firebase');
 
+// FILES
+var twilioWrapper = require("./twilio");
+var db = require('./firebase');
+
+// FIREBASE
+var fbRef = new Firebase('https://betterme-data.firebaseio.com/');
+var remindersRef = fbRef.child('Reminders');
+var followUpsRef = fbRef.child('FollowUp');
+
+var app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/getText', function (req, res) {
@@ -17,4 +27,63 @@ var server = app.listen(3000, function () {
   var port = server.address().port;
 
   console.log('Example app listening at http://%s:%s', host, port);
+});
+
+// CREATE USER
+// var num = '+1234561234';
+// db.createUser(num);
+// db.setName(num, 'Shashank');
+// db.setEmail(num, 'Shashank@gmail.com');
+
+// REMINDER
+// var reminder = {
+//   phoneNumber: '9494194999',
+//   text: 'do yoga',
+//   interval: 86400000, // 24 hours = daily
+//   sendTime: new Date().getTime(),
+//   state: 0, // 0 - reminder, 1 - first follow up, 2 - second follup up, etc
+// }
+// db.createReminder(reminder);
+
+// FOLLOW UP
+// var followup = {
+//   phoneNumber: '9494194999',
+//   text: 'brush teeth',
+//   interval: 300000, // 24 hours = daily
+//   sendTime: new Date().getTime(),
+//   state: 2, // 0 - reminder, 1 - first follow up, 2 - second follup up, etc
+// }
+// db.createFollowUp(followup);
+
+
+var sendRemindersAtTime = function(time){
+  var end = new Date(time);
+  end.setSeconds(end.getSeconds()+60);
+
+  remindersRef
+  .orderByChild('sendTime')
+  // .startAt(time)
+  // .endAt(end)
+  .on('child_added', function(snap){
+    var reminder = snap.val();
+
+    console.log(reminder);
+
+    // send reminder on Twilio
+
+    // create follow up
+
+    // Fix?
+    // snap.ref().remove();
+  });
+};
+
+// var now = new Date();
+// sendRemindersAtTime(now);
+
+var cronJob = cron.job("0 * * * * *", function(){
+  console.log('cron');
+  var now = new Date();
+  // sendRemindersAtTime(now);
+  console.log(now);
 });
